bb7ebfc79ec23ef0f9406ca5b62fd12d61516f76
Rahul kumar maury <rahu7827759@gmail.com>
2024-11-28 13:47:37 +0530

    add eslint error code

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
 };
-
+module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+};
 export default nextConfig;
