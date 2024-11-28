d1bad4ff9fe533291f27bd9f42164c1d6314c1f7
Rahul kumar maury <rahu7827759@gmail.com>
2024-11-28 13:58:08 +0530

    removed import

--- next.config.mjs
+++ next.config.mjs
@@ -2,11 +2,4 @@
 const nextConfig = {
   reactStrictMode: true,
 };
-module.exports = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
-};
 export default nextConfig;
