f89896723b3839825b970541a4ee59efe7db36ff
Rahul kumar maury <rahu7827759@gmail.com>
2024-12-03 17:19:57 +0530

    add icons

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { FaCode } from "react-icons/fa6";
 
 const Skills = () => {
     return (
       <div className="lg:w-100 h-screen py-2 ">
-        <h2 className="text-5xl text-center drblue">Skills</h2>
+        <h2 className="text-5xl text-center drblue flex justify-center font-bold items-center">
+          <span className="mr-3">
+            <FaCode />
+          </span>{" "}
+          Skill<span style={{ color: "rgb(115, 3, 167)" }}>s</span>
+        </h2>
         <div className=""></div>
       </div>
     );
